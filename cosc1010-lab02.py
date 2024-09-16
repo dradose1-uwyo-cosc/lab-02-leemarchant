b3dc127838a59c89f48e69a14cb5e2b5a1f30da2
leemarchant <mjadalie@gmail.com>
2024-09-16 22:03:06 +0000

    completed assignment

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,23 +1,21 @@
-# Your Name Here
+# Lee (Jadalie) Marchant
 # UWYO COSC 1010
-# Submission Date
+# 9/16/2024
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Lab Section: 14 
+# Sources, people worked with, help given to: None
+# 
 
 your_variable_here = "when you see this, replace it with your code"
 
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print("Hello, COSC1010")
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
 print("Your code here")
